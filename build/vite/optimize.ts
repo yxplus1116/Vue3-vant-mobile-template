a57d9e84918c89f0b58f07b2beba04d260db003b
超级烦 <40259902+yxplus1116@users.noreply.github.com>
2024-11-22 18:53:47 +0800

    Initial commit

---
+++ build/vite/optimize.ts
@@ -0,0 +1,26 @@
+const include = [
+  'axios',
+  'echarts',
+  'lodash-es',
+  'resize-detector',
+  'vant/es/cell-group/style/index',
+  'vant/es/popup/style/index',
+  'vant/es/picker/style/index',
+  'vant/es/cell/style/index',
+  'vant/es/switch/style/index',
+  'vant/es/space/style/index',
+  'vant/es/button/style/index',
+  'vant/es/empty/style/index',
+  'vant/es/icon/style/index',
+  'vant/es/stepper/style/index',
+  'vant/es/image/style/index',
+  'vant/es/form/style/index',
+  'vant/es/field/style/index',
+  'vant/es/notify/style/index',
+]
+
+const exclude = [
+  '@iconify-json/carbon',
+]
+
+export { include, exclude }
